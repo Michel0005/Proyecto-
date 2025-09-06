590c79d27c92c8ff852b820276047f1e1e561d07
Michel0005 <A01714887@tec.mx>
2025-09-06 00:19:55 -0600

    Add files via upload

--- calculadora_p100.py
+++ calculadora_p100.py
@@ -7,23 +7,33 @@ BPPP = 1500000 #cantidad de puntos para 1 prestigio
 
 
 #reducir nombres para facilidad de acceso 
+
+#funciones aplicacion 
+def calcular_puntos_faltantes (prestigio_deseado, bp_disponibles):
+    total_requerido = (prestigio_deseado) * (BPPP)
+    bp_faltantes = (total_requerido) - (bp_disponibles)
+    return bp_faltantes
+
+def calcular_puntos_gastados (prestigio_actual, su_nivel):
+    puntos_de_prestigio = (prestigio_actual) * (BPPP)
+    puntos_de_nivel = (puntos_de_prestigio) / (LVM)
+    puntos_actuales_gastados = (su_nivel - 1) * (puntos_de_nivel) 
+    puntos_gastados_en_total = (puntos_de_prestigio) + (puntos_de_nivel) 
+    return puntos_actuales_gastados, puntos_gastados_en_total
+
+
 prestigio_deseado = float(input("ingrese su prestigio deseado: "))
 prestigio_actual = float(input("ingrese su prestigio actual: "))
 su_nivel = float(input("ingrese su nivel actual: "))
 bp_disponibles = float(input("ingrese los bp que tiene actualmente: "))
 
-total_requerido = (prestigio_deseado) * (BPPP)
-bp_faltantes = (total_requerido) - (bp_disponibles)
+#invocacion de funciones 
 
-puntos_de_prestigio = (prestigio_actual) * (BPPP)
-puntos_de_nivel = (puntos_de_prestigio) / (LVM)
-puntos_actuales_gastados = (su_nivel - 1) * (puntos_de_nivel) 
-puntos_gastados_en_total = (puntos_de_prestigio) + (puntos_de_nivel) 
+bp_faltantes = calcular_puntos_faltantes(prestigio_deseado, bp_disponibles)
+puntos_actuales_gastados, puntos_gastados_en_total = calcular_puntos_gastados(prestigio_actual, su_nivel)
 
-#hay que ponerle comas a los numeros para facilidad de lectura 
-
-print(f"la cantidad de puntos faltantes para su prestigo: {bp_faltantes:.2f} ")
-print(f"la cantidad de puntos gastados actualmente es:{puntos_actuales_gastados:.2f}")
-print(f"la cantidad de puntos gastados en total es: {puntos_gastados_en_total:.2f}")
+print(f"la cantidad de puntos faltantes para su prestigo: {bp_faltantes:,.2f} ")
+print(f"la cantidad de puntos gastados actualmente es:{puntos_actuales_gastados:,.2f}")
+print(f"la cantidad de puntos gastados en total es: {puntos_gastados_en_total:,.2f}")
 
 #redccion de lineas de texto debe de ser 75 su no mal recuerdo 
