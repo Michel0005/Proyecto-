3652c36b2ae7fa7fb2099776605008a77db18d43
Michel0005 <A01714887@tec.mx>
2025-10-05 17:02:29 -0600

    Add files via upload

--- calculadora_p100.py
+++ calculadora_p100.py
@@ -8,63 +8,98 @@ BPPP = 1500000 #cantidad de puntos para 1 prestigio
 
 #reducir nombres para facilidad de acceso 
 
-def calcular_puntos_faltantes(prestigio_deseado, bp_disponibles):
-    total_requerido = prestigio_deseado * BPPP
-    bp_faltantes = total_requerido - bp_disponibles
+def calcular_puntos_faltantes(prestigo, bp):
+    total_requerido = prestigo * BPPP
+    bp_faltantes = total_requerido - bp
     return bp_faltantes
 
-def calcular_puntos_gastados(prestigio_actual,  su_nivel):
+def calcular_puntos_gastados(prestigio_a,  su_nivel):
     costo_por_nivel = BPPP / LVM
     puntos_actuales_gastados = (su_nivel - 1) * costo_por_nivel 
-    puntos_gastados_en_total = (prestigio_actual * BPPP) + puntos_actuales_gastados 
+    puntos_gastados_en_total = (prestigio_a * BPPP) + puntos_actuales_gastados 
     return puntos_actuales_gastados, puntos_gastados_en_total 
 
-prestigio_deseado = -1
-while not (0 <= prestigio_deseado <= PMAX):
-    try:
-        prestigio_deseado = float(input("Ingrese su prestigio deseado (0-100): "))
-        if not (0 <= prestigio_deseado <= PMAX):
-            print("El prestigio deseado debe estar entre 0 y 100.")
-    except ValueError:
-        print("Por favor, ingrese un número válido.")
+def obtener_datos_deseados ():
 
-prestigio_actual = -1
-while not (0 <= prestigio_actual <= prestigio_deseado):
-    try:
-        prestigio_actual = float(input(f"Ingrese su prestigio actual (0-{prestigio_deseado}): "))
-        if prestigio_actual < 0: 
-            print("El prestigio actual no puede ser negativo.")
-        elif prestigio_actual > prestigio_deseado:
-            print(f"El prestigio actual no puede ser mayor que el prestigio deseado ({prestigio_deseado}).")
-    except ValueError:
-        print("Por favor, ingrese un número válido.")
-        
-su_nivel = 0 
-while not (1 <= su_nivel <= LVM):
-    try:
-        su_nivel = float(input(f"Ingrese su nivel actual (1-{LVM}): "))
-        if not (1 <= su_nivel <= LVM):
-            print(f"El nivel debe estar entre 1 y {LVM}.")
-    except ValueError:
-        print("Por favor, ingrese un número válido.")
+    prestigo = -1
+    while not (0 <= prestigo <= PMAX):
+        try:
+            prestigo = float(input("Ingrese su prestigio deseado (0-100): "))
+            if not (0 <= prestigo <= PMAX):
+                print("El prestigio deseado debe estar entre 0 y 100.")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
 
-bp_disponibles = -1
-while bp_disponibles < 0:
+    prestigio_a = -1
+    while not (0 <= prestigio_a <= prestigo):
+        try:
+            prestigio_a = float(input(f"Ingrese su prestigio actual (0-{prestigo}): "))
+            if prestigio_a < 0: 
+                print("El prestigio actual no puede ser negativo.")
+            elif prestigio_a > prestigo:
+                print(f"El prestigio actual no puede ser mayor que el prestigio deseado ({prestigo}).")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+            
+    su_nivel = 0 
+    while not (1 <= su_nivel <= LVM):
+        try:
+            su_nivel = float(input(f"Ingrese su nivel actual (1-{LVM}): "))
+            if not (1 <= su_nivel <= LVM):
+                print(f"El nivel debe estar entre 1 y {LVM}.")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+
+    bp = -1
+    while bp < 0:
+        try:
+            bp = float(input("Ingrese los BP que tiene actualmente: "))
+            if bp < 0:
+                print("Los BP disponibles no pueden ser negativos.")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+    
+    return {
+       "prestigio_deseado": prestigo,
+        "prestigio_actual": prestigio_a,
+        "su_nivel": su_nivel,
+        "bp_disponibles": bp
+    }
+
+lista_resultados = []
+personajes = 0
+while personajes <= 0:
     try:
-        bp_disponibles = float(input("Ingrese los BP que tiene actualmente: "))
-        if bp_disponibles < 0:
-            print("Los BP disponibles no pueden ser negativos.")
+        personajes = int(input("\n con cuantos personajes va a realizar el calculo?"))
+        if personajes <= 0 :
+            print ("ingresa un numero positivo.")
     except ValueError:
-        print("Por favor, ingrese un número válido.")
+        print ("entrada no valida")
+    
+for indice in range(personajes):
+    print (f"\n personaje {indice + 1}")
 
-bp_faltantes = calcular_puntos_faltantes(prestigio_deseado, bp_disponibles)
+    datos = obtener_datos_deseados()
 
-puntos_actuales_gastados, puntos_gastados_en_total = calcular_puntos_gastados(prestigio_actual, su_nivel)
+    bp_faltantes = calcular_puntos_faltantes(datos["prestigio_deseado"], datos["bp_disponibles"])
+    puntos_actuales_gastados, puntos_gastados_en_total = calcular_puntos_gastados(datos["prestigio_actual"], datos["su_nivel"])
+    
+    resultado = {
+        "cálculo": indice  + 1, 
+        "p_deseado": datos["prestigio_deseado"],
+        "p_actual": datos["prestigio_actual"],
+        "bp_faltantes": bp_faltantes,
+        "bp_gastados_total": puntos_gastados_en_total
+    }
+    lista_resultados.append(resultado)
+
 
 print ("\n" + "="*50 )
 print ("Resultados:")
 print("="*50)
 
-print(f"La cantidad de puntos faltantes para su prestigio: {bp_faltantes:,.2f}")
-print(f"La cantidad de puntos gastados actualmente es: {puntos_actuales_gastados:,.2f}")
-print(f"La cantidad de puntos gastados en total es: {puntos_gastados_en_total:,.2f}")   
+for resultado in lista_resultados:
+    print(f"\nResultados pesonaje #{resultado['cálculo']}:")
+    print(f"  Prestigio deseado: P{resultado['p_deseado']:.0f} (Desde P{resultado['p_actual']:.0f})")
+    print(f"  Puntos Faltantes: {resultado['bp_faltantes']:,.2f}")
+    print(f"  Puntos Gastados Totales: {resultado['bp_gastados_total']:,.2f}")
